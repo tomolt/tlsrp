46e973a4d440b0fb413040accf2e8e4447500e6d
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2022-09-09 18:07:15 +0200

    Even more style changes

--- tlsrp.c
+++ tlsrp.c
@@ -79,12 +79,12 @@ static int
 serve(int serverfd, int clientfd, struct tls *clientconn)
 {
 	struct pollfd pfd[] = {
-		{serverfd, POLLIN | POLLOUT, 0},
-		{clientfd, POLLIN | POLLOUT, 0}
+		{ serverfd, POLLIN | POLLOUT, 0 },
+		{ clientfd, POLLIN | POLLOUT, 0 }
 	};
 
-	char clibuf[BUFSIZ] = {0};
-	char serbuf[BUFSIZ] = {0};
+	char clibuf[BUFSIZ] = { 0 };
+	char serbuf[BUFSIZ] = { 0 };
 
 	char *cliptr = NULL, *serptr = NULL;
 
@@ -97,7 +97,7 @@ serve(int serverfd, int clientfd, struct tls *clientconn)
 
 		if ((pfd[CLIENT].revents & POLLIN) && clicount == 0) {
 			clicount = tls_read(clientconn, clibuf, BUFSIZ);
-			if (clicount == -1) {
+			if (clicount < 0) {
 				tdie(clientconn, "client read failed:");
 				return -2;
 			} else if (clicount == TLS_WANT_POLLIN) {
@@ -111,7 +111,7 @@ serve(int serverfd, int clientfd, struct tls *clientconn)
 
 		if ((pfd[SERVER].revents & POLLIN) && sercount == 0) {
 			sercount = read(serverfd, serbuf, BUFSIZ);
-			if (sercount == -1) {
+			if (sercount < 0) {
 				die("server read failed:");
 				return -3;
 			}
@@ -120,7 +120,7 @@ serve(int serverfd, int clientfd, struct tls *clientconn)
 
 		if ((pfd[SERVER].revents & POLLOUT) && clicount > 0) {
 			written = write(serverfd, cliptr, clicount);
-			if (written == -1)
+			if (written < 0)
 				die("failed to write:");
 			clicount -= written;
 			cliptr += written;
@@ -128,7 +128,7 @@ serve(int serverfd, int clientfd, struct tls *clientconn)
 
 		if ((pfd[CLIENT].revents & POLLOUT) && sercount > 0) {
 			written = tls_write(clientconn, serptr, sercount);
-			if (written == -1)
+			if (written < 0)
 				tdie(clientconn, "failed tls_write:");
 			else if (written == TLS_WANT_POLLIN) {
 				pfd[CLIENT].events = POLLIN;
@@ -151,53 +151,36 @@ serve(int serverfd, int clientfd, struct tls *clientconn)
 }
 
 int 
-main(int argc, char* argv[])
+main(int argc, char **argv)
 {
 	int serverfd = 0, clientfd = 0, bindfd = 0;
-	struct sockaddr_storage client_sa = {0};
+	struct sockaddr_storage client_sa = { 0 };
 	struct tls_config *config;
 	struct tls *toclient, *conn;
 	socklen_t client_sa_len = 0;
-	char *backpath = NULL,
+	pid_t pid;
+	char *backpath  = NULL,
 	     *frontpath = NULL,
 	     *backhost  = NULL,
-	     *fronthost  = NULL,
-	     *backport = NULL,
+	     *fronthost = NULL,
+	     *backport  = NULL,
 	     *frontport = NULL,
-	     *ca_path = NULL,
-	     *cert_path = NULL,
-	     *key_path = NULL;
+	     *capath    = NULL,
+	     *certpath  = NULL,
+	     *keypath   = NULL;
 
 	ARGBEGIN {
-	case 'a':
-		ca_path = EARGF(usage());
-		break;
-	case 'h':
-		backhost = EARGF(usage());
-		break;
-	case 'H':
-		fronthost = EARGF(usage());
-		break;
-	case 'k':
-		key_path = EARGF(usage());
-		break;
-	case 'p':
-		backport = EARGF(usage());
-		break;
-	case 'P':
-		frontport = EARGF(usage());
-		break;
-	case 'r':
-		cert_path = EARGF(usage());
-		break;
-	case 'u':
-		backpath = EARGF(usage());
-		break;
-	case 'U':
-		frontpath = EARGF(usage());
-		break;
+	case 'a': capath    = EARGF(usage()); break;
+	case 'h': backhost  = EARGF(usage()); break;
+	case 'H': fronthost = EARGF(usage()); break;
+	case 'k': keypath   = EARGF(usage()); break;
+	case 'p': backport  = EARGF(usage()); break;
+	case 'P': frontport = EARGF(usage()); break;
+	case 'r': certpath  = EARGF(usage()); break;
+	case 'u': backpath  = EARGF(usage()); break;
+	case 'U': frontpath = EARGF(usage()); break;
 	case 'v':
-		printf("%s " VERSION "\n", argv0);
+		printf("%s %s\n", argv0, VERSION);
 		exit(0);
 		break;
 	case '?':
@@ -215,37 +198,31 @@ main(int argc, char* argv[])
 	if ((frontpath && fronthost) || !(frontpath || frontport))
 		die("can only receive on unix socket xor network socket");
 
-	if (!ca_path || !cert_path || !key_path)
+	if (!capath || !certpath || !keypath)
 		die("must provide ca_path, cert_path and key_path");
 
-	if ((config = tls_config_new()) == NULL)
+	if (!(config = tls_config_new()))
 		tcdie(config, "failed to get tls config:");
 
-	if (tls_config_set_protocols(config, protocols) == -1)
+	if (tls_config_set_protocols(config, protocols) < 0)
 		tcdie(config, "failed to set protocols:");
-
-	if (tls_config_set_ciphers(config, ciphers) == -1)
+	if (tls_config_set_ciphers(config, ciphers) < 0)
 		tcdie(config, "failed to set ciphers:");
-
-	if (tls_config_set_dheparams(config, dheparams) == -1)
+	if (tls_config_set_dheparams(config, dheparams) < 0)
 		tcdie(config, "failed to set dheparams:");
-
-	if (tls_config_set_ecdhecurves(config, ecdhecurves) == -1)
+	if (tls_config_set_ecdhecurves(config, ecdhecurves) < 0)
 		tcdie(config, "failed to set ecdhecurves:");
-
-	if (tls_config_set_ca_file(config, ca_path) == -1)
+	if (tls_config_set_ca_file(config, capath) < 0)
 		tcdie(config, "failed to load ca file:");
-
-	if (tls_config_set_cert_file(config, cert_path) == -1)
+	if (tls_config_set_cert_file(config, certpath) < 0)
 		tcdie(config, "failed to load cert file:");
-
-	if (tls_config_set_key_file(config, key_path) == -1)
+	if (tls_config_set_key_file(config, keypath) < 0)
 		tcdie(config, "failed to load key file:");
 
-	if ((toclient = tls_server()) == NULL)
+	if (!(toclient = tls_server()))
 		die("failed to create server context");
 
-	if ((tls_configure(toclient, config)) == -1)
+	if ((tls_configure(toclient, config)) < 0)
 		tdie(toclient, "failed to configure server:");
 
 	tls_config_free(config);
@@ -255,16 +232,14 @@ main(int argc, char* argv[])
 	else
 		bindfd = networksocket(fronthost, frontport, bind);
 
-	if (listen(bindfd, BACKLOG) == -1) {
+	if (listen(bindfd, BACKLOG) < 0) {
 		close(bindfd);
 		die("could not start listen:");
 	}
 
-	pid_t pid;
-
 	while (1) {
-		if ((clientfd = accept(bindfd, (struct sockaddr*) &client_sa, 
-						&client_sa_len)) == -1) {
+		if ((clientfd = accept(bindfd, (struct sockaddr *)&client_sa, 
+						&client_sa_len)) < 0) {
 			warn("could not accept connection:");
 			continue;
 		}
@@ -276,16 +251,13 @@ main(int argc, char* argv[])
 			else
 				serverfd = networksocket(backhost, backport, connect);
 
-			if (tls_accept_socket(toclient, &conn, clientfd) == -1) {
+			if (tls_accept_socket(toclient, &conn, clientfd) < 0) {
 				warn("tls_accept_socket: %s", tls_error(toclient));
-				goto tlsfail;
+			} else {
+				if (serverfd)
+					serve(serverfd, clientfd, conn);
+				tls_close(conn);
 			}
-
-			if (serverfd)
-				serve(serverfd, clientfd, conn);
-
-			tls_close(conn);
-tlsfail:
 			close(serverfd);
 			close(clientfd);
 			close(bindfd);
